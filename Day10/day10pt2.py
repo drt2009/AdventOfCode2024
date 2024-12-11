03aa08095e039ad3d2f4a73860ea8543827acbb4
Don Tobias <dtobiasalmont09@gmail.com>
2024-12-11 08:44:54 -0500

    Completed Day 10

--- Day10/day10pt2.py
+++ Day10/day10pt2.py
@@ -1,26 +1,23 @@
 path_map = []
 total_counts = 0 
 
-def check_for_next_number(_current_x, _current_y,_trail_end_locations=None):
+def check_for_next_number(_current_x, _current_y):
     global path_map
-    if _trail_end_locations is None:
-        _trail_end_locations = set()
-        
     _current_number = path_map[_current_x][_current_y]
+    _full_path = 0
     if _current_number == 9:
-        _trail_end_locations.add((_current_x,_current_y))
-        return _trail_end_locations
+        return 1
     
     if _current_x-1 >= 0 and path_map[_current_x-1][_current_y] == _current_number+1:
-        check_for_next_number(_current_x-1, _current_y,_trail_end_locations)
+        _full_path+=check_for_next_number(_current_x-1, _current_y)
     if _current_x+1 < len(path_map) and path_map[_current_x+1][_current_y] == _current_number+1:
-        check_for_next_number(_current_x+1, _current_y,_trail_end_locations)
+        _full_path+=check_for_next_number(_current_x+1, _current_y)
     if _current_y-1 >= 0 and path_map[_current_x][_current_y-1] == _current_number+1:
-        check_for_next_number(_current_x, _current_y-1,_trail_end_locations)
+        _full_path+=check_for_next_number(_current_x, _current_y-1)
     if _current_y+1 < len(path_map[_current_x]) and path_map[_current_x][_current_y+1] == _current_number+1:
-        check_for_next_number(_current_x, _current_y+1,_trail_end_locations)
+        _full_path+=check_for_next_number(_current_x, _current_y+1)
 
-    return _trail_end_locations
+    return _full_path
 
 
 with open("day10.txt","r") as file:
@@ -30,6 +27,6 @@ with open("day10.txt","r") as file:
 for i in range(len(path_map)):
     for j in range (len(path_map[i])):
         if path_map[i][j] == 0:
-            total_counts+=len(check_for_next_number(i,j))
+            total_counts+=check_for_next_number(i,j)
 
 print(total_counts)
